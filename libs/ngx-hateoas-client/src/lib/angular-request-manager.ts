cb9b362f77177bb220d4b11be2b2be6a37530ce2
Daniel Murrmann <daniel.murrmann@live.de>
2023-04-25 11:25:13 +0200

    Added xsrf and cookie handling

--- libs/ngx-hateoas-client/src/lib/angular-request-manager.ts
+++ libs/ngx-hateoas-client/src/lib/angular-request-manager.ts
@@ -18,10 +18,21 @@ export class AngularRequestManager extends RequestManager {
         }
         
         let requestPromise = new Promise((resolve, reject) => {
-            let headers = new HttpHeaders().set('Content-Type', 'application/json');
+            let headers = new HttpHeaders()
+                              .set('Content-Type', 'application/json')
+                              .set('X-Requested-With', 'XmlHttpRequest');
+
 
             if(securityToken) {
                 headers = headers.set('Authorization', 'Bearer ' + securityToken);
+            }
+
+            if(method !== 'GET') {
+                const xsrfToken = this.getXsrfCookie();
+
+                if(xsrfToken) {
+                    headers = headers.set('X-XSRF-TOKEN', xsrfToken);
+                }
             }
 
             this._httpClient.request(method, url, { body, headers }).subscribe({ 
@@ -29,10 +40,21 @@ export class AngularRequestManager extends RequestManager {
                     resolve(response); 
                 },
                 error: response => {
+                    if(response.status === 401) {
+                        // Need to redirect to sign in 
+                        const currentUrl = window.location.href;
+                        window.location.href = '/login?redirectUrl=' + encodeURIComponent(currentUrl);
+                    }
                     reject(response);
                 } });
         });
 
         return await requestPromise;
+    }
+
+    private getXsrfCookie(cookieName?: string) {
+        let name=cookieName || 'XSRF-TOKEN';
+        const o=document.cookie.split(";").map(t=>t.trim()).filter(t=>t.startsWith(name+"="));
+        return 0===o.length?null:decodeURIComponent(o[0].split("=")[1]);
     }
 }
