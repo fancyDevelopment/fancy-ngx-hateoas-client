cc194cf2bfe5e0aeac3b88b6c0935e1406a1bd05
Daniel Murrmann <daniel.murrmann@live.de>
2023-09-17 13:04:06 +0200

    Version update

--- libs/fancy-ngx-hateoas-client/src/public-api.ts
+++ libs/fancy-ngx-hateoas-client/src/public-api.ts
@@ -1,6 +1,6 @@
-import { HateoasClient } from 'fancy-hateoas-client';
+import { HateoasClient, ResourceBase } from 'fancy-hateoas-client';
 
 export * from './lib/provide';
 export * from './lib/view-model-reloader';
 export * from './lib/view-base';
-export { HateoasClient }
+export { HateoasClient, ResourceBase }
